f341ea04abcd472f8343535fe380e9df76a4ae16
jeny3g <mirandajean2009@gmail.com>
2023-01-09 14:42:22 -0300

    struct: :construction_worker: installing axios and error class

---
+++ src/application/services/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { APP_KEY_HEADER, APP_TOKEN_HEADER } from "../constants/api-path-paymee";
+
+
+const apiPayMee = axios.create({
+  baseURL: process.env.PAYMEE_API_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    [APP_KEY_HEADER]: process.env.PAYMEE_API_KEY,
+    [APP_TOKEN_HEADER]: process.env.PAYMEE_API_SECRET,
+  },
+});
+
+export { apiPayMee };
