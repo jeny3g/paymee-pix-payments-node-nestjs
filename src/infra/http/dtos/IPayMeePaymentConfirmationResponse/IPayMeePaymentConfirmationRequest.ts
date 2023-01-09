ff47055db2d570d8d6f41fa68b49bf4117daf22c
jeny3g <mirandajean2009@gmail.com>
2023-01-09 14:32:47 -0300

    feat: :sparkles: creating paymee dtos

---
+++ src/infra/http/dtos/IPayMeePaymentConfirmationResponse/IPayMeePaymentConfirmationRequest.ts
@@ -0,0 +1,12 @@
+import { IPaymeeShopper } from "../IPaymeeShopper";
+
+export type IPayMeePaymentConfirmationRequest = {
+  saleToken: string;
+  referenceCode: string;
+  currency: string;
+  amount: number;
+
+  payMeeShopper: IPaymeeShopper;
+  date: string;
+  newStatus: string;
+};
