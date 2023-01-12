fd4b1e9edf768dfa78893404d297e078a43e1f99
jeny3g <mirandajean2009@gmail.com>
2023-01-12 15:14:44 -0300

    feat: :sparkles: inject Request header in nestjs service

--- src/shared/services/paymee/paymee.service.ts
+++ src/shared/services/paymee/paymee.service.ts
@@ -1,21 +1,28 @@
 import { PayMeeServiceRepository } from '@application/repositories/paymee-service-repository';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Request } from '@nestjs/common';
 import {
   APP_KEY_HEADER,
   APP_TOKEN_HEADER,
 } from '@shared/constants/api-path-paymee';
 import axios, { AxiosInstance } from 'axios';
+import { REQUEST } from '@nestjs/core';
+
 
 @Injectable()
 export class PayMeeService implements PayMeeServiceRepository {
+  constructor(@Inject(REQUEST) private request: Request) {}
+
   public api(): AxiosInstance {
+    const apiKey = this.request.headers['x-api-key'];
+    const apiToken = this.request.headers['x-api-token'];
+
     return axios.create({
       baseURL: process.env.PAYMEE_API_URL,
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        [APP_KEY_HEADER]: process.env.PAYMEE_API_KEY_SANDBOX,
-        [APP_TOKEN_HEADER]: process.env.PAYMEE_API_SECRET_SANDBOX,
+        [APP_KEY_HEADER]: apiKey || process.env.PAYMEE_API_KEY_SANDBOX,
+        [APP_TOKEN_HEADER]: apiToken || process.env.PAYMEE_API_SECRET_SANDBOX,
       },
     });
   }
